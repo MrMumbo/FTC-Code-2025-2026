f8a4dceccbb94d07d5d23d3c716861c8c28d076f
MrMumbo <danielemerson321@gmail.com>
2025-11-19 16:59:35 -0800

    AprilTag Distance Power
    Added the Code for shootPower at different distances. No testing yet though

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2025-11-19T09:58:07.954-0800";
+  public static final String APP_BUILD_TIME = "2025-11-19T16:43:22.396-0800";
 }
