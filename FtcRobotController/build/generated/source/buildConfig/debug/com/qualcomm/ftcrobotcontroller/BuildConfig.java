e3f30ac66b8f22bd3015033bb499b5ed56103ebe
MrMumbo <danielemerson321@gmail.com>
2025-12-03 23:46:47 -0800

    Folder Added - FCSSDemo
    Fix to seperate different bots

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2025-12-01T17:15:24.815-0800";
+  public static final String APP_BUILD_TIME = "2025-12-03T23:42:56.156-0800";
 }
